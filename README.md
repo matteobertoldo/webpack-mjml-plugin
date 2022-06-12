a8e5682f3a396f9e10dfa92940310ec8b8b175e4
Matteo Bertoldo <mrbert93@gmail.com>
2022-06-12 19:44:58 +0200

    chore: added plugin and configured the basic API

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h1 align="center">Webpack MJML Plugin</h1>
 
-<p align="center">Webpack MJML Plugin for compiling MJML files.</p>
+<p align="center">Webpack <a href="https://mjml.io">MJML</a> Plugin for compiling MJML files.</p>
 
 <p align="center">
   <a href="https://github.com/prettier/prettier">
@@ -9,24 +9,71 @@
   <a href="https://snyk.io/test/github/matteobertoldo/mjml-webpack-plugin?targetFile=package.json">
     <img src="https://snyk.io/test/github/matteobertoldo/mjml-webpack-plugin/badge.svg?targetFile=package.json" alt="Known Vulnerabilities" />
   </a>
-  <a href="https://www.npmjs.com/package/mjm-webpack-plugin">
-    <img src="https://img.shields.io/npm/v/mjm-webpack-plugin.svg" />
+  <a href="https://app.travis-ci.com/matteobertoldo/mjml-webpack-plugin">
+    <img src="https://app.travis-ci.com/matteobertoldo/mjml-webpack-plugin.svg?branch=master" alt="Travis CI Build">
+  </a>
+  <a href="https://www.npmjs.com/package/mjml-webpack-plugin">
+    <img src="https://img.shields.io/npm/v/mjml-webpack-plugin.svg" alt="NPM Package Version" />
   </a>
 </p>
 
 ---
 
-## Installation
+## Installation :gift:
 
 ```sh
 npm install webpack-mjml-plugin
 ```
 
-## Contributing
+## Usage :joystick:
+
+In your `webpack.config.js` simply:
+
+```javascript
+const MJMLPlugin = require('webpack-mjml-plugin');
+
+module.exports = {
+  // ...
+  plugins: [
+    new MJMLPlugin('src/to/mjml', {
+      extension: '.html',
+      outputPath: path.resolve(__dirname, 'dist/to/mjml')
+    });
+  ]
+};
+```
+
+## API :bee:
+
+The plugin supports all `options` that can be found in [this documentation](https://documentation.mjml.io/#inside-node-js).
+
+```js
+// webpack.config.js
+
+module.exports = {
+  plugins: [
+    new MJMLPlugin(inputPath, {
+      extension: options.extension,
+      outputPath: options.outputPath,
+      // MJML options (https://documentation.mjml.io/#inside-node-js)
+      filePath: path.resolve(__dirname, 'dist/to/mjml'),
+      keepComments: false
+    })
+  ]
+};
+```
+
+| Parameter            |   Type   |     Default     | Description                                         |
+| -------------------- | :------: | :-------------: | --------------------------------------------------- |
+| `inputPath`          | `string` |   `undefined`   | The path where `.mjml` files are located.           |
+| `options.extensions` | `string` |     `.html`     | The default output extension.                       |
+| `options.outputPath` | `string` | `process.cwd()` | The path where compiled files should be written to. |
+
+## Contributing :busts_in_silhouette:
 
 Please read [CONTRIBUTING.md](https://github.com/matteobertoldo/webpack-mjml-plugin/blob/main/CONTRIBUTING.md) for details on code of conduct, and the process for submitting pull requests.
 
-## License
+## License :balance_scale:
 
 [![npm](https://img.shields.io/npm/l/mjm-webpack-plugin.svg)](https://www.npmjs.com/package/mjm-webpack-plugin)
 
---
+++ src/webpack-mjml-store.js
@@ -0,0 +1,136 @@
+const fs = require('fs-extra');
+const glob = require('glob');
+const mjml2html = require('mjml');
+
+/**
+ * @type {{extension: string, outputPath: string}}
+ */
+const defaultOptions = {
+  extension: '.html',
+  outputPath: process.cwd()
+};
+
+/**
+ * @param inputPath - {String}
+ * @param options
+ * @constructor
+ */
+const WebpackMjmlStore = function (inputPath, options) {
+  this.inputPath = inputPath.replace(/\\/g, '/');
+  this.options = { ...defaultOptions, ...options };
+  this.options.outputPath = this.options.outputPath.replace(/\\/g, '/');
+};
+
+/**
+ * @param compiler
+ */
+WebpackMjmlStore.prototype.apply = function (compiler) {
+  compiler.hooks.emit.tapAsync('webpack-mjml-store', function (compilation, callback) {
+    fs.ensureDirSync(this.options.outputPath);
+
+    glob(`${this.inputPath}/**/*.mjml`, function (err, files) {
+      if (err) {
+        throw err;
+      }
+
+      if (!files.length) {
+        return callback();
+      }
+
+      const tasks = [];
+      for (const fileKey in files) {
+        const file = files[fileKey];
+        if (compilation.fileDependencies.add) {
+          compilation.fileDependencies.add(file);
+        } else {
+          compilation.fileDependencies.push(file);
+        }
+
+        const outputFile = file
+          .replace(this.inputPath, this.options.outputPath)
+          .replace('.mjml', this.options.extension);
+        tasks.push(this.handleFile(file, outputFile));
+      }
+
+      Promise.all(tasks).then(callback());
+    });
+  });
+};
+
+/**
+ * @param file
+ * @param outputFile
+ * @returns {Promise}
+ */
+WebpackMjmlStore.prototype.handleFile = function (file, outputFile) {
+  return new Promise(function (resolve) {
+    this.convertFile(file)
+      .then((contents) => this.ensureFileExists(outputFile, contents))
+      .then((contents) => this.writeFile(outputFile, contents))
+      .then(resolve());
+  });
+};
+
+/**
+ * @param file
+ * @returns {Promise}
+ */
+WebpackMjmlStore.prototype.convertFile = function (file) {
+  const that = this;
+  return new Promise(function (resolve) {
+    fs.readFile(file, 'utf8', function (err, contents) {
+      if (err) {
+        throw err;
+      }
+
+      const response = mjml2html(contents, that.options);
+      if (response.errors.length) {
+        console.log('\x1b[36m', `MJML Warnings in file "${file}":`, '\x1b[0m');
+      }
+
+      for (const errorKey in response.errors) {
+        console.log('  -  ', response.errors[errorKey].formattedMessage);
+      }
+
+      resolve(response.html);
+    });
+  });
+};
+
+/**
+ * @param file
+ * @param contents
+ * @returns {Promise}
+ */
+WebpackMjmlStore.prototype.writeFile = function (file, contents) {
+  return new Promise(function (resolve) {
+    fs.writeFile(file, contents, function (err) {
+      if (err) {
+        throw err;
+      }
+      resolve(true);
+    });
+  });
+};
+
+/**
+ * @param file
+ * @param contents
+ * @returns {Promise}
+ */
+WebpackMjmlStore.prototype.ensureFileExists = function (file, contents) {
+  return new Promise(function (resolve) {
+    fs.ensureFile(file, function (err) {
+      if (err) {
+        throw err;
+      }
+
+      resolve(contents);
+    });
+  });
+};
+
+/**
+ * @type {WebpackMjmlStore}
+ */
+module.exports = WebpackMjmlStore;
