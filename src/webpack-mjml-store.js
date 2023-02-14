4bb6bacf2ed6237a6ff4dfa232e846d1ec4c4491
Matteo Bertoldo <mrbert93@gmail.com>
2023-02-14 14:33:47 +0100

    fix: fixed the throw error when the dist folder don't exist

--- src/webpack-mjml-store.js
+++ src/webpack-mjml-store.js
@@ -174,7 +174,7 @@ WebpackMjmlStore.prototype.readFile = function (file) {
     try {
       fs.readFile(file, 'utf8', function (err, data) {
         if (err) {
-          throw err;
+          resolve(true);
         }
         resolve(data);
       });
